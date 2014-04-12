70555ce477f8d5458a0ee76c6c5f425a9cd18ebc
Dmitry Cherkassov <dcherkassov@gmail.com>
2014-04-13 01:26:17 +0400

    update table

--- kurkumator.el
+++ kurkumator.el
@@ -1,6 +1,6 @@
 (provide 'kurkumator)
 
-(setq ko-table ["k" "ле" "yay" "лойс" "маман" "кококо" "кукарек" "зафорсил"])
+(setq ko-table ["k" "ле" "yay" "лойс" "маман" "аутист" "кукарек" "зафорсил"])
 
 (defun make-ko-regexp (word-len sw start end)
   (let ((rx (format "\\b[[:alpha:]]\\{%d\\}\\b" word-len)))
@@ -14,4 +14,3 @@
       (make-ko-regexp (+ 1 number) (aref ko-table number) start end))))
 
 ;; select the region and call M-x kokoify
-
