6df9fc57d0a873217d9c1d9c27f494629bad36f5
Dmitry Cherkassov <dcherkassov@gmail.com>
2014-04-14 02:39:33 +0400

    kmajor rework: use levenstein edit diff

--- kurkumator.el
+++ kurkumator.el
@@ -1,17 +1,40 @@
-(provide 'kurkumator)
+(require 'levenshtein)
 
-(setq ko-table ["k" "ле" "yay" "лойс" "свежо" "аутист" "кукарек" "зафорсил"])
+(setq ko-list (list "путин" "рашка"  "пук" "свежо" "аутизм" "быдло" "говно" "скатывать" "скатываешь" "сыч" "чухан" "спайс" "куркума" "поссал" "нассал" "обосрал" "для быдла" "лох" "лол" "лул" "хуле" "лях" "мюсли" "бнв" "дедфуд" "личкрафт" "тян" "кун" "форсить" "форс" "отсос" "соснул" "гей" "прыщи"  "куркума" "твою" "анус" "ле" "yay" "лойс" "маман" "аутист" "кукарек" "зафорсил"))
 
-(defun make-ko-regexp (word-len sw start end)
-  (let ((rx (format "\\b[а-яА-Я]\\{%d\\}\\b" word-len)))
-    ;; (rx (format "\\b[[:alpha:]]\\{%d\\}\\b" word-len))
-    (replace-regexp rx sw nil start end)
-    rx))
+(defvar *max-diff* 10)
+
+(defun find-nearest (word)
+  (setq res nil)
+  (setq ldiff *max-diff*)
+  (dolist (cw ko-list res)
+    (let ((cdiff (levenshtein-distance word cw)))
+      (when (> ldiff cdiff)
+        (progn 
+          (setq res cw)
+          (setq ldiff cdiff))))))
+
+(defun koko-word (start end)
+  (goto-char start)
+  (let* ((cand (buffer-substring start end))
+         (res (find-nearest cand)))
+    (when res
+      (kill-region start end)
+      (insert res))))
 
 (defun kokoify (start end)
   (interactive "r")
   (save-excursion 
-    (dotimes (number (length ko-table) nil) 
-      (make-ko-regexp (+ 1 number) (aref ko-table number) start end))))
+    (setq foundp t)
+    (goto-char start)
+    (while foundp
+      (setq foundp 
+            (re-search-forward "\\b\\([а-яА-Я]+\\)\\b" end t))
+      (if foundp
+          (progn
+            (koko-word (match-beginning 1) (match-end 1))
+            (goto-char (match-end 1)))))))
 
 ;; select the region and call M-x kokoify
+
+(provide 'kurkumator)
